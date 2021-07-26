ae31a84297c9909d2fc1bc3f55bf5e8f55209a38
Ashutosh <ashutoshkumar7921@gmail.com>
2021-07-26 20:39:07 +0530

    Heroku setup

--- server.js
+++ server.js
@@ -3,11 +3,14 @@ const express= require('express');
 const mongoose = require("mongoose");
 const NoteApirouter = require("./route/api/Notes");
 const cors = require("cors");
+const path = require("path");
 
 const app = express();
 app.use(cors());
 
 app.use(express.urlencoded({extended:true}));
+
+app.use(express.static(path.resolve(__dirname, "./client/build")));
 
 mongoose
 .connect(process.env.DB_URL, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: true })
@@ -22,6 +25,12 @@ app.get("/", (req, res)=>{
 });
 
 const port = process.env.PORT || 5000;
+
 app.listen(port, () => {
     console.log(`Server started on port ${port}`);
 });
+
+app.get("*", function (request, response) {
+    response.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
+  });
+  
